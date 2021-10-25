25f77de15c389cbb2dbc564d5552fc6c134ca804
ogatarou <92582956+ogatarou@users.noreply.github.com>
2021-10-26 02:11:13 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const quiz = [
+    {
+        question: 'お酢に卵を殻ごと入れると卵はどうなるでしょうか？',
+        answers:  [
+            '透明な卵になる',
+            '鏡のようになんでもうつる卵になる',
+            '卵が溶けてなくなる',
+            '卵が石のように堅くなる'
+        ],
+        correct: '透明な卵になる'
+    },{
+        question: 'しゃっくりはある調味料を舐めると止まります。ある調味料とはなんでしょう？',
+        answers:  [
+            'お酢',
+            '砂糖',
+            '醤油',
+            '塩'
+        ],
+        correct: '砂糖'
+    },{  
+        question: '甘くておいしい羊羹ですが、羊羹はもともとあるものを煮込んだスープのコトでした。あるスープとはなんでしょう？',
+        answers:  [
+            '牛の血',
+            '牛乳',
+            '羊の肉',
+            '馬の肉'
+        ],
+        correct: '羊の肉'
+    }
+];
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
+    let buttonIndex = 0;
+    while(buttonIndex < buttonLength){
+        $button [buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex]
+        buttonIndex++;
+    }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+    if(quiz[quizIndex].correct === e.target.textContent){
+        window.alert('正解！');
+        score++;
+      }else{
+        window.alert('不正解！');
+    }
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        //問題数がまだあればこちらを実行
+        setupQuiz();
+    }else{
+        //問題数がもうなければこちらを実行
+        window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+    }
+
+};
+
+//ボタンをクリックしたら正誤判定
+let handlerIndex = 0;
+while(handlerIndex < buttonLength){
+    $button[handlerIndex].addEventListener('click',(e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
